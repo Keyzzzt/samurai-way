e2682cba69c7eb94a4006ca862a639ae03b967fc
Keyzzzt <keyzzzt@gmail.com>
2022-11-25 22:21:30 +0200

    18 - DONE

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import './App.css';
 import { Profile } from './components/Profile/Profile';
 import { Header } from './components/Header/Header';
 import { Navbar } from './components/Navbar/Navbar';
+import { Dialogs } from './components/Dialogs/Dialogs';
 
 function App() {
   return (
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <Profile />
+      <div className="app">
+        <div className='header'>
+          <Header />
+        </div>
+        <div className='navbar'>
+          <Navbar />    
+        </div>
+        {/* <Profile /> */}
+        <div className='dialogs'>
+          <Dialogs />
+        </div>
     </div>
   );
 }
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,11 @@
+import s from './Dialogs.module.css'
+
+export const Dialogs = () => {
+  console.log(s);
+  
+  return (
+    <div>
+      Dialogs
+    </div>
+  )
+}
