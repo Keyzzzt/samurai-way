7da955116a9c738587d9a2d9ce8ec0f349e536b3
Keyzzzt <keyzzzt@gmail.com>
2022-11-26 15:36:02 +0200

    21 - Dialogs Page, DONE

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,25 @@
 import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
-  console.log(s);
   
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={`${s.dialog} ${s.active}`}>
+          Mark
+        </div>
+        <div className={s.dialog}>
+          Igor
+        </div>
+        <div className={s.dialog}>
+          Kote
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hola</div>
+        <div className={s.message}>Hej</div>
+      </div>
     </div>
   )
 }
