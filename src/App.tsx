a719627f6f564aa8d6d1e8632cc0b6c86059fae4
Keyzzzt <keyzzzt@gmail.com>
2022-11-26 10:06:18 +0200

    19 - react-router, DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import './App.css';
-import { Profile } from './components/Profile/Profile';
-import { Header } from './components/Header/Header';
-import { Navbar } from './components/Navbar/Navbar';
-import { Dialogs } from './components/Dialogs/Dialogs';
+import React from "react"
+import "./App.css"
+import { Profile } from "./components/Profile/Profile"
+import { Header } from "./components/Header/Header"
+import { Navbar } from "./components/Navbar/Navbar"
+import { Dialogs } from "./components/Dialogs/Dialogs"
+import { BrowserRouter, Route } from "react-router-dom"
 
 function App() {
   return (
-      <div className="app">
-        <div className='header'>
-          <Header />
-        </div>
-        <div className='navbar'>
-          <Navbar />    
-        </div>
-        {/* <Profile /> */}
-        <div className='dialogs'>
-          <Dialogs />
-        </div>
+    <BrowserRouter>
+          <div className="app">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="profile">
+        <Route path='/dialogs' component={Dialogs} />
+        <Route path='/profile' component={Profile} />
+      </div>
     </div>
-  );
+    </BrowserRouter>
+
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,16 +6,16 @@ export const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href="#">Profile</a>
+        <a href="/profile">Profile</a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="#">Messages</a>
+        <a href="/dialogs">Dialogs</a>
       </div>
       <div className={s.item}>
-        <a href="#">News</a>
+        <a href="/news">News</a>
       </div>
       <div className={s.item}>
-        <a href="#">Music</a>
+        <a href="/music">Music</a>
       </div>
     </nav>
   )
