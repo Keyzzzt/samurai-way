1f28d102f5b4e8cab8892c03d2f23d0916ac8d13
Keyzzzt <keyzzzt@gmail.com>
2023-02-08 16:34:18 +0200

    54 DONE

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,36 +1,39 @@
 import { connect } from "react-redux"
-import {FC} from 'react'
+import {FC, Component} from 'react'
 import { Dispatch } from "redux"
-import { actions, UsersPageType } from "../../redux/reducers/usersReducer"
+import { actions, UsersPageType, UserType } from "../../redux/reducers/usersReducer"
 import { StateType } from "../../redux/store"
+import axios from "axios"
 
 
-const Users: FC<UsersProps> = ({usersPage,follow, unFollow}) => {
-    return (
-        <div>
-            {usersPage.users.map(u => (
+class UsersClass extends Component<UsersProps> {
+    componentDidMount(){
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(res => this.props.setUsers(res.data.items))
+    }
+    render(){
+        return (
+            <div>
+            {this.props.usersPage.users.map(u => (
                 <div key={u.id}>
                     <div>
-                        <img src="" alt="" />
-                        {u.followed ? (<button onClick={() => unFollow(u.id)}>Unfollow</button>) : (<button onClick={() => follow(u.id)}>Follow</button>)}
-                    </div>
-                    <div>
-                        <span>{u.fullname}</span>
-                        <span>{u.location.city} {u.location.country}</span>
-                        
+                        <span>{u.name}</span>
+                        <img src={u.photos.small ? u.photos.small : ''} alt="" />
+                        {u.followed ? (<button onClick={() => this.props.unFollow(u.id)}>Unfollow</button>) : (<button onClick={() => this.props.follow(u.id)}>Follow</button>)}
                     </div>
                 </div>
             ))}
         </div>
-    )
+        )
+    }
 }
-
 type MSType = {
     usersPage: UsersPageType
 }
 type MDType = {
     follow: (userId: string) => void 
     unFollow: (userId: string) => void 
+    setUsers: (users: UserType[]) => void
     
 }
 export type UsersProps = MSType & MDType
@@ -41,7 +44,8 @@ const ms = (state: StateType): MSType => {
 const md = (dispatch: Dispatch): MDType => {
     return {
         follow: (userId: string) => {dispatch(actions.followAC(userId))},
-        unFollow: (userId: string) => {dispatch(actions.unFollowAC(userId))}
+        unFollow: (userId: string) => {dispatch(actions.unFollowAC(userId))},
+        setUsers: (users: UserType[]) => {dispatch(actions.setUsers(users))},
     }
 }
-export const UsersContainer = connect(ms, md)(Users)
+export const UsersContainer = connect(ms, md)(UsersClass)
